e5bab8f2946251d820238c84e8b377c4ecea0c67
Praveen <praveen-kumar-rr@users.noreply.github.com>
2017-08-29 16:31:17 +0530

    Add files via upload

---
+++ puzzle1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+# n = 100
+
+# r = 100
+
+# e = 10
+# h = 1
+# c = 0.125
+
+
+
+# ne + nh + nc = 100
+
+# ne*e + nh*h + nc*c = 100
+
+# ne*10 + nh + nc/8 = 100
+
+for e in xrange(1,99):
+    for h in xrange(1,99):
+        for c in xrange(1,99):
+            if sum( [e,h,c] ) == 100 and sum( [ e*10,h,c*0.125] ) == 100:
+            	print 'E : {}, H : {}, C : {}'.format( e,h,c )
